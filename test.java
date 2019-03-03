be75e120cc647e1237d1f469c460e2056624b65e
Shikha kumari <35431975+kshikha818@users.noreply.github.com>
2019-03-03 22:16:30 +0530

    Update test.java

--- test.java
+++ test.java
@@ -5,11 +5,11 @@ public class test {
 
     public static void main(String args[ ]) {
     Map<Integer,String> states = new HashMap<Integer,String>();
-    states.put(1, "INDIA");
-    states.put(2, "USA");
+    states.put(1, "cancer");
+    states.put(2, "brain tumor");
 
     states.put(3, null);    // Okay
-    states.put(null,"UK");
+    states.put(null,"asthama");
 
     System.out.println(states.get(1));
     System.out.println(states.get(2));
